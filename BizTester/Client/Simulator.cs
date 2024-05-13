dbc4093837872c81b45c1ece25528564ff393db4
Parsa, Kourosh KP3023_SA <kourosh.parsa@fraserhealth.ca>
2024-05-13 16:34:52 -0700

    removing the hl7 file dependency

--- BizTester/Client/Simulator.cs
+++ BizTester/Client/Simulator.cs
@@ -6,19 +6,22 @@ namespace BizTester.Client
 {
     internal class Simulator
     {
-        const string sampleDataPath = "../../../sample_data.hl7v2";
         private static int count = 0;
         private static HL7.Dotnetcore.Message msg = null;
         public static String GetHL7Message()
         {
             count += 1;
+           
             if (msg == null)
             {
-                string content = File.ReadAllText(sampleDataPath);
+                string content = @"MSH|^~\\&|AcmeHIS|StJohn|CATH|StJohn|20061019172719||ORM^O01|MSGID12349876|P|2.3
+PID|||20301||Durden^Tyler^^^Mr.||19700312|M|||88 Punchward Dr.^^Los Angeles^CA^11221^USA|||||||
+PV1||O|OP^^||||4652^Paulson^Robert|||OP|||||||||9|||||||||||||||||||||||||20061019172717|20061019172718
+ORC|NW|20061019172719
+OBR|1|20061019172719||76770^Ultrasound: retroperitoneal^C4|||12349876";
                 msg = new HL7.Dotnetcore.Message(content);
                 msg.ParseMessage();
             }
-
             string messageControlID = $"MSGID{count}";
             msg.AddSegmentMSH("SendingApp", "Hospital1", "ReceivingApp", "Hospital2", "", "ORM^O01", messageControlID, "P", "2.3");
             msg.SetValue("PID.5.5", ""); // no prefix
