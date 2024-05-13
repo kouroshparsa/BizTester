bdb361bc9b42400064cb1c6f5edd2985616ce532
kouroshparsa <kouroshtheking@gmail.com>
2024-05-13 16:38:56 -0700

    removing hl7 file dependency

--- WinFormsApp1/Client/Simulator.cs
+++ WinFormsApp1/Client/Simulator.cs
@@ -4,7 +4,6 @@ namespace BizTester.Client
 {
     internal class Simulator
     {
-        const string sampleDataPath = "../../../../sample_data.hl7v2";
         private static int count = 0;
         private static HL7.Dotnetcore.Message? msg = null;
         public static String GetHL7Message()
@@ -12,7 +11,11 @@ namespace BizTester.Client
             count += 1;
             if (msg == null)
             {
-                string content = File.ReadAllText(sampleDataPath);
+                string content = @"MSH|^~\\&|AcmeHIS|StJohn|CATH|StJohn|20061019172719||ORM^O01|MSGID12349876|P|2.3
+PID|||20301||Durden^Tyler^^^Mr.||19700312|M|||88 Punchward Dr.^^Los Angeles^CA^11221^USA|||||||
+PV1||O|OP^^||||4652^Paulson^Robert|||OP|||||||||9|||||||||||||||||||||||||20061019172717|20061019172718
+ORC|NW|20061019172719
+OBR|1|20061019172719||76770^Ultrasound: retroperitoneal^C4|||12349876";
                 msg = new HL7.Dotnetcore.Message(content);
                 msg.ParseMessage();
             }
